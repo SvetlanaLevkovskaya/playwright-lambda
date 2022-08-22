0a5bb4758c032ba8125d57384a4246277b2fbce5
Svetlana Levkovskaya <levkovskayase@gmail.com>
2022-08-22 20:11:05 +0300

    update

--- tests/dropdown.test.ts
+++ tests/dropdown.test.ts
@@ -11,3 +11,21 @@ test("handling dropdown", async ({ page }) => {
   await page.waitForTimeout(3000)
 
 });
+
+test.only("multiple objects",async ({ page }) => {
+  await page.goto("https://www.lambdatest.com/selenium-playground/select-dropdown-demo");
+  const scrolldown = page.locator("//div[text()='Multi Select List Demo']");
+  await scrolldown.scrollIntoViewIfNeeded();
+  await page.selectOption("#multi-select", [{
+    label: "New York"
+  },
+  {
+    index: 2
+  }, {
+    value: "Washington"
+  }
+]);
+
+await page.waitForTimeout(3000);
+
+});
