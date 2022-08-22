43fa0b026ce70346930be2a7757d202b5c977570
Svetlana Levkovskaya <levkovskayase@gmail.com>
2022-08-22 20:35:32 +0300

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,10 @@ const config: PlaywrightTestConfig = {
   use: {
     headless: false,
     screenshot: "on",
-    video : "on"
+    video: "on",
+    launchOptions: {
+      slowMo: 1000
+    }
   },
   retries: 0,
   reporter: [["dot"], ["json", {
